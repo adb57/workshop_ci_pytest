f300c066803389dc40449fe34ac501ba4e2312d3
adb57 <107985695+adb57@users.noreply.github.com>
2022-06-22 10:32:21 +0200

    Create first unit test

---
+++ test/test_functions.py
@@ -0,0 +1,4 @@
+from functions import add
+
+def test_add():
+  assert add(3, 4) == 7 # assert is central format for unit tests. if after statement there is a value that is true, then nothing happens. if false an exception at line 4 happens
