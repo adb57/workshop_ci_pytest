1a4609924a69dfd36a9dce6b045cab67e43b086a
adb57 <107985695+adb57@users.noreply.github.com>
2022-06-22 11:30:03 +0200

    Printing example

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,4 +1,10 @@
 from functions import add
+import numpy as np
 
 def test_add():
   assert add(3, 4) == 7 # assert is central format for unit tests. if after statement there is a value that is true, then nothing happens. if false an exception at line 4 happens
+
+def test_show_printing():
+    matrix = np.random.rand(5, 5)
+    print(matrix)
+    assert False 
